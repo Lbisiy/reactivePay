655f6cb4514b7d7721f3f248fc379b3558112ddd
Marat <maratsafin.let@gmail.com>
2023-11-24 14:39:53 +0100

    commit

---
+++ reactivePay/get_order/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GetOrderConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'get_order'
---
+++ reactivePay/get_order/views.py
@@ -0,0 +1,26 @@
+import requests
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import View
+
+
+class GetOrderView(View):
+    MERCHANT_PRIVATE_KEY = '1dab4b92513e63571052'
+    SANDBOX_URL = 'https://business.reactivepay.com'
+
+    def get(self, request, order_number):
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {self.MERCHANT_PRIVATE_KEY}'
+        }
+
+        response = requests.get(f'{self.SANDBOX_URL}/api/v1/payments/order/{order_number}', headers=headers)
+
+        if response.status_code == 200:
+            data = response.json()
+            print(order_number)
+            return HttpResponse(f'<html><body><span>Order {data}</body></html>')
+        else:
+            return HttpResponse(
+                f'<html><body><span>Something gone wrong: {response.status_code, response.text}</span></body></html>')
